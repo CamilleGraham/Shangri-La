e5c27ccf69a015f2cc8e012a4ff5b58ed6b1f10b
CamilleGraham <camillergraham@yahoo.ca>
2025-12-06 21:22:47 -0700

    setting up bones
    setting up structure adding pages

---
+++ shangri-la.js
@@ -0,0 +1,8 @@
+            
+            
+const togglebButton = document.getElementById('toggle');
+const mainMenu = document.getElementById('menu');
+
+togglebButton.addEventListener('click',function(){
+mainMenu.classList.toggle('active');
+}); 
